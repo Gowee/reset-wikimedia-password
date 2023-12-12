8ea31dbcfbe3f99033107278bf8950d1d3e87074
Hung-I <whygowe@gmail.com>
2023-12-12 23:06:39 +0800

    feat: fastapi -> flask for wsgi support

--- service/app.py
+++ service/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from typing import Annotated, Optional
-from fastapi import FastAPI, Header, Depends, HTTPException
-from fastapi.responses import HTMLResponse
+# from fastapi import FastAPI, Header, Depends, HTTPException
+# from fastapi.responses import HTMLResponse
 from mwclient import Site
 from asyncer import asyncify
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from flask import Flask, abort, request
+
 
 class Settings(BaseSettings):
     inbox_address: Optional[str] = None
@@ -15,11 +17,11 @@ class Settings(BaseSettings):
     wikimedia_password: Optional[str] = None
     api_token: Optional[str] = None
 
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
-app = FastAPI()
+app = Flask(__name__)
 
 with open("index.html", "r") as f:
     index_html = f.read()
@@ -28,26 +30,31 @@ site = Site(settings.wikimedia_site)
 if settings.wikimedia_username:
     site.login(settings.wikimedia_username, settings.wikimedia_password)
 
-@asyncify
+
 def api_resetpassword(email=None, user=None):
     assert email or user
-    return site.api("resetpassword", email=email, user=user, token=site.get_token('csrf'))
-
-async def protected(x_api_token: Annotated[str| None, Header()] = None):
-    if settings.api_token and x_api_token != settings.api_token:
-        raise HTTPException(status_code=403, detail="Invalid token")
+    return site.api(
+        "resetpassword", email=email, user=user, token=site.get_token("csrf")
+    )
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.route("/")
 def root():
     return index_html
 
-@app.get("/get-email-addresses")
+
+@app.route("/get-email-addresses")
 def get_email_address():
-    return {'inbox': settings.inbox_address, 'contact': settings.contact_address}
+    return {"inbox": settings.inbox_address, "contact": settings.contact_address}
 
-@app.get("/reset-password")
-async def reset_password(email: str, _: Annotated[None, Depends(protected)]):
-    return await api_resetpassword(email)
 
-app = app
+@app.route("/reset-password")
+def reset_password():
+    token = request.headers.get("x-api-token", None)
+    if settings.api_token and token != settings.api_token:
+        abort(403)
+
+    email = request.args.get("email", None)
+    if not email:
+        abort(422)
+    return api_resetpassword(email)
